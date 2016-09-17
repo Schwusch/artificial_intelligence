ee921d027be5c93136221c2d09ceaf84bdd43586
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-09-17 19:06:44 +0200

    Refactoring

--- assignment_1/src/Utilities.java
+++ assignment_1/src/Utilities.java
@@ -2,13 +2,7 @@
  * Created by Jonathan Böcker on 2016-09-12.
  */
 class Utilities {
-
-    /**
-     * Clones a matrix
-     * @param src The matrix to be cloned
-     * @return The new matrix
-     */
-    static int[][] cloneMatrix(int[][] src) {
+    static int[][] cloneArray(int[][] src) {
         int length = src.length;
         int[][] target = new int[length][src[0].length];
         for (int i = 0; i < length; i++) {
@@ -17,16 +11,8 @@ class Utilities {
         return target;
     }
 
-    /**
-     * Evaluates a move on an Othelloboard and changes bricks according to Othello rules
-     *
-     * @param start The board as it is when someone made a move
-     * @param move The move that was made
-     * @param player Human or AI move?
-     * @return The board after rules applied
-     */
     static int[][] calculateBoardChange(int[][] start, OthelloCoordinate move, int player) {
-        int[][] afterMove = cloneMatrix(start);
+        int[][] afterMove = cloneArray(start);
         if(start[move.getRow()][move.getCol()] != OthelloGUI.NONE) {
             System.out.print("Error: making move on a none empty space");
 
@@ -38,138 +24,55 @@ class Utilities {
             afterMove[currentRow][currentCol] = player;
 
             // Check in west direction
-            currentCol--;
-            while(currentCol >= 0) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                }else if(afterMove[currentRow][currentCol] == player) {
-                    for(; currentCol < move.getCol(); currentCol++) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-
-                    break;
-                }
-
-                currentCol--;
-            }
-            currentCol = move.getCol();
+            afterMove = Utilities.lookInDirection(0, -1, move, afterMove, player);
 
             // Check in east direction
-            currentCol++;
-            while (currentCol < OthelloGUI.COLS) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if(afterMove[currentRow][currentCol] == player) {
-                    for(; currentCol > move.getCol(); currentCol--) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-
-                    break;
-                }
-                currentCol++;
-            }
-            currentCol = move.getCol();
+            afterMove = Utilities.lookInDirection(0, 1, move, afterMove, player);
 
             // Check in the north direction
-            currentRow--;
-            while (currentRow >= 0) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if (afterMove[currentRow][currentCol] == player) {
-                    for(;currentRow < move.getRow(); currentRow++) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentRow--;
-            }
-            currentRow = move.getRow();
+            afterMove = Utilities.lookInDirection(-1, 0, move, afterMove, player);
 
             // Check in south direction
-            currentRow++;
-            while(currentRow < OthelloGUI.ROWS) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if (afterMove[currentRow][currentCol] == player) {
-                    for (; currentRow > move.getRow(); currentRow--) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentRow++;
-            }
-            currentRow = move.getRow();
+            afterMove = Utilities.lookInDirection(1, 0, move, afterMove, player);
 
             // Check in north-west direction
-            currentCol--;
-            currentRow--;
-            while ( currentCol >= 0 && currentRow >= 0) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if (afterMove[currentRow][currentCol] == player) {
-                    for(;currentRow < move.getRow() && currentCol < move.getCol(); currentRow++, currentCol++) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentCol--;
-                currentRow--;
-            }
-            currentRow = move.getRow();
-            currentCol = move.getCol();
+            afterMove = Utilities.lookInDirection(-1, -1, move, afterMove, player);
 
             // Check in south-west direction
-            currentCol--;
-            currentRow++;
-            while ( currentCol >= 0 && currentRow < OthelloGUI.ROWS) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                }else if (afterMove[currentRow][currentCol] == player) {
-                    for(; currentRow > move.getRow() && currentCol < move.getCol(); currentRow--, currentCol++) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentCol--;
-                currentRow++;
-            }
-            currentRow = move.getRow();
-            currentCol = move.getCol();
+            afterMove = Utilities.lookInDirection(1, -1, move, afterMove, player);
 
             // Check in south-east direction
-            currentCol++;
-            currentRow++;
-            while (currentCol < OthelloGUI.COLS && currentRow < OthelloGUI.ROWS) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if (afterMove[currentRow][currentCol] == player) {
-                    for(; currentRow > move.getRow() && currentCol > move.getCol(); currentRow--, currentCol--) {
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentCol++;
-                currentRow++;
-            }
-            currentRow = move.getRow();
-            currentCol = move.getCol();
+            afterMove = Utilities.lookInDirection(1, 1, move, afterMove, player);
 
             // Check in north-east direction
-            currentCol++;
-            currentRow--;
-            while (currentCol < OthelloGUI.COLS && currentRow >= 0) {
-                if(afterMove[currentRow][currentCol] == OthelloGUI.NONE) {
-                    break;
-                } else if (afterMove[currentRow][currentCol] == player) {
-                    for (; currentRow < move.getRow() && currentCol > move.getCol(); currentRow++, currentCol--){
-                        afterMove[currentRow][currentCol] = player;
-                    }
-                    break;
-                }
-                currentCol++;
-                currentRow--;
-            }
+            afterMove = Utilities.lookInDirection(-1, 1, move, afterMove, player);
         }
         return afterMove;
+    }
+
+    private static int[][] lookInDirection(int deltaRow, int deltaCol, OthelloCoordinate move, int[][] grid, int player) {
+        int currentRow = move.getRow();
+        int currentCol = move.getCol();
+
+        currentCol += deltaCol;
+        currentRow += deltaRow;
+        while (currentCol < OthelloGUI.COLS && currentCol >= 0 && currentRow < OthelloGUI.ROWS && currentRow >= 0) {
+            if(grid[currentRow][currentCol] == OthelloGUI.NONE) {
+                break;
+            } else if (grid[currentRow][currentCol] == player) {
+                while (currentCol != move.getCol() || currentRow != move.getRow()) {
+                    grid[currentRow][currentCol] = player;
+
+                    currentCol -= deltaCol;
+                    currentRow -= deltaRow;
+                }
+
+                break;
+            }
+            currentCol += deltaCol;
+            currentRow += deltaRow;
+        }
+
+        return grid;
     }
 }
