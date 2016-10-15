ecef16f5544a7a69796af15e9bc43bf758b72cb1
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-10-15 16:45:47 +0200

    ¿por que no los dos?

--- assignment_1/convertTEXtoMD.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env python
-import os
-os.system('pandoc -f latex -t markdown_github -o README.md Othello_assignment.tex --bibliography=references.bib')
-print("Converted Othello_assignment.tex to README.md")
