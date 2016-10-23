bcc0f4581333b97fef0db27cc9b8a092e5780d36
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2016-10-23 04:03:40 +0200

    An almost endless loop incoming

--- assignment_3/Knapsack/src/com/mah/KnapSackSolver.java
+++ assignment_3/Knapsack/src/com/mah/KnapSackSolver.java
@@ -32,10 +32,32 @@ public class KnapSackSolver {
         ArrayList<ProblemWrapper> neighbors = new ArrayList<>();
         ArrayList<KnapSack> knapSacks = wrapper.getKnapsacks();
 
+        // Try all permutations of knapsack item moves to find neighbors
         for (KnapSack sack1 : knapSacks) {
             for (KnapSack sack2 : knapSacks) {
                 if (sack1 != sack2) {
-                    // TODO: Flytta paket och ha sig för att göra nya ProblemWrappers
+                    Iterator<Item> sack1ItemIter = ((LinkedList<Item>)sack1.getItems().clone()).iterator();
+                    while (sack1ItemIter.hasNext()) {
+                        Item item = sack1ItemIter.next();
+                        // If the item fits the other sack
+                        if (sack2.addItem(item)) {
+                            sack1ItemIter.remove();
+                            // Add another item to the first sack
+                            Iterator<Item> itemLeftIter = ((LinkedList<Item>)wrapper.getItemsLeft().clone()).iterator();
+                            while (itemLeftIter.hasNext()) {
+                                if (sack1.addItem(itemLeftIter.next())) {
+                                    itemLeftIter.remove();
+                                    // TODO: This is a valid neighbor?
+                                }
+                            }
+                        // If the item does NOT fit the other sack, remove one, then add it
+                        } else {
+                            Iterator<Item> sack2ItemIter = ((LinkedList<Item>)sack2.getItems().clone()).iterator();
+                            while (sack2ItemIter.hasNext()) {
+
+                            }
+                        }
+                    }
                 }
             }
         }
